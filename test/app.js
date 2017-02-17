3caa5da401d3ba3cf376c7bf8d362609e224b369
Brendan Myers <brendan.myers@heliansys.com>
2017-02-18 00:20:32 +1100

    Fixed mock for direct-replyTo

--- test/app.js
+++ test/app.js
@@ -1,7 +1,7 @@
 const assert = require('assert')
 const amqplib = require('amqplib-mocks');
 const proxyquire = require('proxyquire');
-const tibbar = proxyquire( '../lib/application', { _amqplib: amqplib } );
+const tibbar = proxyquire( '../lib/application', { amqplib: amqplib } );
 
 describe('app', () => {
 	it('should be a function', () => {
@@ -25,34 +25,32 @@ describe('app', () => {
 		assert.equal(typeof app._worker.queues['/a'], 'object');
 	});
 
-	it('should create and open a queue when connected', function() {
+	it('should create and open a queue when connected', function(done) {
 		const app = new tibbar.default();
-		app.connect('amqp://localhost').then(function() {
-			app.accept('/b');
-			assert.equal(typeof app._worker.queues['/b'], 'object');
-			app.disconnect();
+		app.connect('should create and open a queue when connected').then(() => {
+			app.accept('/');
+			assert.equal(typeof app._worker.queues['/'], 'object');
+			done();
 		});
 	});
 
 	it('should create a queue when not connected, and open when connected', function(done) {
 		const app = new tibbar.default();
-		app.accept('/c');
-		app.connect('amqp://localhost').then(function() {
-			assert.equal(typeof app._worker.queues['/c'], 'object');
-			app.disconnect();
+		app.accept('/');
+		app.connect('should create a queue when not connected, and open when connected').then(() => {
+			assert.equal(typeof app._worker.queues['/'], 'object');
 			done();
 		});
 	});
 
 	it('should call an endpoint, and not expect a response, when using cast', function(done) {
 		const app = new tibbar.default();
-		app.accept('/d', function(req, res) {
+		app.accept('/', function(req, res) {
 			res.ack();
-			app.disconnect();
 			done();
 		});
-		app.connect('amqp://localhost').then(function() {
-			app.cast('/d');
+		app.connect('should call an endpoint, and not expect a response, when using cast').then(function() {
+			app.cast('/');
 		});
 	});
 
@@ -61,30 +59,38 @@ describe('app', () => {
 		assert.throws(function() { app.cast('/') });
 	});
 
-	it('should call an enpoint, and expect a response, when using call', function(done) {
+	it('should call an enpoint, and expect a response, when using call', done => {
 		const app = new tibbar.default();
-		app.accept('/e', function(req, res) {
+		app.accept('/', (req, res) => {
 			res.ack().send();
 		});
-		app.connect('amqp://localhost').then(function() {
-			app.call('/e').then(function(res) {
+		app.connect('should call an enpoint, and expect a response, when using call').then(() => {
+			// --- This is require as amqplib-mocks doesn't support Rabbit's directReplyTo
+			app._worker.ch.assertQueue('amq.rabbitmq.reply-to');
+			app._client.ch.consume(
+				app._options.client.replyTo,
+				msg => app._client.ch.emitter.emit(msg.properties.correlationId, msg),
+				app._options.client.consume
+			);
+			// ---
+
+			app.call('/').then(res => {
 				done();
-				app.disconnect();
 			});
 		});
 	});
 
-	it('should throw an error if call is called, and not connected', function() {
+	it('should throw an error if call is called, and not connected', () => {
 		const app = new tibbar.default();
-		assert.throws(function() { app.call('/') });
+		assert.throws(() => { app.call('/') });
 	});
 
-	it('should call an enpoint, and timeout if no response is sent, when using call', function(done) {
+	it('should call an enpoint, and timeout if no response is sent, when using call', done => {
 		const app = new tibbar.default();
-		app.accept('/f');
-		app.connect('amqp://localhost').then(function() {
-			app.call('/f').then(function(res) {
-			}).catch(function(error) {
+		app.accept('/');
+		app.connect('should call an enpoint, and timeout if no response is sent, when using call').then(() => {
+			app.call('/').then(res => {
+			}).catch(error => {
 				done();
 				app.disconnect();
 			});
