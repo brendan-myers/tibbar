5fe7843034578dc2f511c56a2c748b1b02028474
Brendan Myers <brendan.myers@heliansys.com>
2017-02-17 16:41:04 +1100

    Removed commented-out code

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-/*
-const worker = require('./lib/worker.js');
-
-exports.worker = function(options) {
-	return new worker.default(options);
-}
-
-const client = require('./lib/client.js');
-
-exports.client = function(options) {
-	return new client.default(options);
-}*/
-/*!
- * tibbar
- * Copyright(c) 2016 Brendan Myers
- * MIT Licensed
- */
-
-//'use strict';
-
 const application = require('./lib/application');
 
 exports = module.exports = createApplication;
